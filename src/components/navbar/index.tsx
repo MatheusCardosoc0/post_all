112e842ac0dfd135ccd2132a604bc6dc1908eb25
MatheusCardosoc0 <matheuscardosoc905@gmail.com>
2023-05-26 11:35:39 -0300

    preparando para deploy

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -16,11 +16,17 @@ const Navbar = () => {
         drop-shadow-[0px_4px_4px_black]
       "
     >
-      <Link href={'/post/dashboard'}>
+      <Link
+        href={'/post/dashboard'}
+        className="rounded-full bg-white p-2 text-xl font-bold hover:bg-green-400"
+      >
         <h1>PostAll</h1>
       </Link>
 
-      <Link href={'/post/create'}>
+      <Link
+        href={'/post/create'}
+        className="rounded-full bg-white p-2 text-2xl font-bold hover:bg-green-400"
+      >
         <span>Publicar</span>
       </Link>
     </nav>
