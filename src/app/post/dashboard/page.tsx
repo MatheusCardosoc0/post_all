611f6a144de884bf6ecb3342456d8a7122fd60d7
MatheusCardosoc0 <matheuscardosoc905@gmail.com>
2023-05-26 12:10:32 -0300

    teste fetch

--- src/app/post/dashboard/page.tsx
+++ src/app/post/dashboard/page.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
-import { getPosts } from '../../functions/getPosts'
 import PostCard from '@/components/PostCard'
 
 export default async function dashboardPage() {
-  const posts = await getPosts()
+  const posts: any[] = await fetch(
+    'https://node-deploy-k49o.onrender.com/posts',
+    {
+      next: {
+        revalidate: 3,
+      },
+    },
+  )
 
   return (
     <div className="flex h-full w-full flex-col items-center gap-8 pb-20 ">
